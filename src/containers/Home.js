8dc3e2bf67f2694ddf560e5c800e04fadc0e1ca3
Jason Sturges <jason@jasonsturges.com>
2019-08-17 00:07:52 -0500

    Boilerplate

---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from '../layouts/PageLayout';
+import './Home.scss';
+
+const Home = () => (
+  <PageLayout>
+    <h1 className='site-title'>Babylon.js Roxik Sharikua</h1>
+  </PageLayout>
+);
+
+export default Home;
