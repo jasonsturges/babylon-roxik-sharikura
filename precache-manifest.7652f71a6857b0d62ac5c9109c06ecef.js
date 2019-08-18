8b791e22911268c496f01d1ccfca036c04ba88e0
Jason Sturges <jason@jasonsturges.com>
2019-08-18 00:37:19 -0500

    Updates

---
+++ precache-manifest.7652f71a6857b0d62ac5c9109c06ecef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faddd9b310684dc92171e12c3d3eb59c",
+    "url": "/babylon-roxik-sharikua/index.html"
+  },
+  {
+    "revision": "e40b22b9f3910fed1e56",
+    "url": "/babylon-roxik-sharikua/static/css/main.b93f734d.chunk.css"
+  },
+  {
+    "revision": "6ca98a9ede2605fdd185",
+    "url": "/babylon-roxik-sharikua/static/js/2.c5293e30.chunk.js"
+  },
+  {
+    "revision": "e40b22b9f3910fed1e56",
+    "url": "/babylon-roxik-sharikua/static/js/main.3f6f1fae.chunk.js"
+  },
+  {
+    "revision": "e98c6075d7a419590e9c",
+    "url": "/babylon-roxik-sharikua/static/js/runtime~main.9eb6e67d.js"
+  }
+]);
