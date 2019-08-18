c5a39fa348bc77dd35c3acad2a5e20515632a762
Jason Sturges <jason@jasonsturges.com>
2019-08-18 00:40:10 -0500

    GitHub pages

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './containers/Home';
 
 const Main = () => (
-  <BrowserRouter basename="/babylon-react-example">
+  <BrowserRouter basename="/babylon-roxik-sharikura">
     <Switch>
       <Route exact path='/' component={Home}></Route>
     </Switch>
